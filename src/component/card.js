19f8f5f52d9a015b7f752ce214be2ffc00a14c11
neha2124 <nehashukla2124@gmail.com>
2023-04-05 02:43:15 +0530

    ...

--- src/component/card.js
+++ src/component/card.js
@@ -13,7 +13,7 @@ const Card = () => {
         
         
         return(
-          <div className='card text-yellow-100 p-3 border-b-emerald-700 mt-3 cursor-pointer w-52  overflow-hidden h-80  ' key={e.id}>
+          <div className='card text-yellow-100 p-3 border-b-emerald-700 mt-3 cursor-pointer w-52  overflow-hidden h-80 m-4  ' key={e.id}>
             <img src={`https://picsum.photos/200?random=$%7Bpost.id%7D${e.id}`} alt="" />
             <p ><span className='text-gray-400 px-1 ' >user Id:</span>{e.id}</p>
             <p ><span className='text-gray-400 px-1 ' >Title:</span>{e.title.slice(0,15)}...</p>
